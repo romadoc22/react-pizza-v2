21052ac1c0e661dd0a3af1eae99bf81f55314751
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-09-11 22:32:29 +0300

    функція мар

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -4,27 +4,31 @@ function PizzBlock({title, price}) {
 
   const [pizzaCount, setPizzaCount]= useState(0)
 
-  return (<div class="pizza-block">
+  const onClickAdd = () => {
+    setPizzaCount(pizzaCount+1)
+  }
+
+  return (<div className="pizza-block">
   <img
-    class="pizza-block__image"
+    className="pizza-block__image"
     src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
     alt="Pizza"
   />
-  <h4 class="pizza-block__title">{title}</h4>
-  <div class="pizza-block__selector">
+  <h4 className="pizza-block__title">{title}</h4>
+  <div className="pizza-block__selector">
     <ul>
-      <li class="active">тонкое</li>
+      <li className="active">тонкое</li>
       <li>традиционное</li>
     </ul>
     <ul>
-      <li class="active">26 см.</li>
+      <li className="active">26 см.</li>
       <li>30 см.</li>
       <li>40 см.</li>
     </ul>
   </div>
-  <div class="pizza-block__bottom">
-    <div class="pizza-block__price">от {price} ₽</div>
-    <button class="button button--outline button--add">
+  <div className="pizza-block__bottom">
+    <div className="pizza-block__price">от {price} ₽</div>
+    <button onClick={onClickAdd} className="button button--outline button--add">
       <svg
         width="12"
         height="12"
