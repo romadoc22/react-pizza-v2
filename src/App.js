7d093da3a658e16f20f0972138e8f417a40d4d7d
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-09-14 22:11:14 +0300

    fetch mockapi

--- src/App.js
+++ src/App.js
@@ -20,7 +20,15 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
-              <PizzaBlock title={obj.title} price={obj.price} />
+              <PizzaBlock
+                key={obj.id}
+                {...obj}
+                // title={obj.title}
+                // price={obj.price}
+                // imageUrl={obj.imageUrl}
+                // sizes={obj.sizes}
+                // types={obj.types}
+              />
             ))}
           </div>
         </div>
